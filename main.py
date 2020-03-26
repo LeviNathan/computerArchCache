d2be96fc81a92c645eaaea50e9dace98c539c2d9
Nathan Chan <nathanchinchan@gmail.com>
2020-03-26 11:32:44 -0500

    Initial error checks and renaming of files.

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+import sys
+import os
+
+"""
+Your simulator will have the following input parameters: 
+1. –f <trace file name> [ name of text file with the trace ] 
+2. –s <cache size in KB> [ 1 KB to 8 MB ] 
+3. –b <block size>  [ 4 bytes to 64 bytes ] 
+4. –a <associativity>   [ 1, 2, 4, 8, 16 ] 
+5. –r <replacement policy> [ RR or RND or LRU for bonus points] 
+
+Sample command lines: 
+    Sim.exe –f trace1.txt –s 1024 –b 16 –a 2 –r RR 
+That would read the trace file named “trace1.txt”, 
+configure a total cache size of 1 MB with a block size of 16 bytes/block. 
+It would be 2-way set associative and use a replacement policy of Round Robin. 
+We will assume a write-through policy.  Cost: $0.05 / KB 
+"""
+
+if len(sys.argv) != 11 and len(sys.argv) != 9:
+    sys.exit("Error: Invalid number of arguments")
+
+file = sys.argv[sys.argv.index('-f') + 1]
+if not os.path.exists(file):
+    sys.exit("Error: File does not exist.")
+
+cacheSize = sys.argv[sys.argv.index('-s') + 1]
+try:
+    cacheSize = int(cacheSize)
+except ValueError:
+    sys.exit("Error: Invalid cache size, make sure it is an integer.")
+
+blockSize = sys.argv[sys.argv.index('-b') + 1]
+try:
+    blockSize = int(blockSize)
+except ValueError:
+    sys.exit("Error: Invalid block size, make sure it is an integer.")
+
+associativity = sys.argv[sys.argv.index('-a') + 1]
+try:
+    associativity = int(associativity)
+except ValueError:
+    sys.exit("Error: Invalid associativity, make sure it is an integer.")
+
+replacement = None
+if '-r' in sys.argv:
+    replacement = sys.argv[sys.argv.index('-r') + 1]
+    if replacement not in ['RR', 'RND', 'LRU']:
+        sys.exit("Error: Invalid replacement policy. (RR, RND, or LRU)")
 
